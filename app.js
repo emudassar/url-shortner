1b48afd7b44c4f636d89bff37dace35ade6574f4
emudassar <contacmudassar@gmail.com>
2025-08-02 13:54:59 +0500

    basic setup aand work is completed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { readFile } from "fs/promises";
 import { createServer } from "http";
 import path from "path";
-import crypto from "crypto"
+import crypto from "crypto";
+import { writeFile } from "fs/promises";
 
 const PORT = 3001;
+const DATA_FILE = path.join("data", "links.json")
 
 const serveFile = async (res, filePath, contentType) => {
   try {
@@ -16,6 +18,23 @@ const serveFile = async (res, filePath, contentType) => {
   }
 };
 
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data)
+  } catch (error) {
+    if(error.code === "ENOENT") {
+      await writeFile(DATA_FILE, JSON.stringify({}))
+      return {}
+    }
+    throw error
+  }
+};
+
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links))
+}
+
 const server = createServer(async (req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
@@ -26,18 +45,31 @@ const server = createServer(async (req, res) => {
   }
 
   if (req.method === "POST" && req.url === "/shorten") {
-    const body = "";
+    const links = await loadLinks();
+
+    let body = "";
     req.on("data", (chunk) => {
-      body += chunk;
+      return body += chunk;
     });
-    req.end("end", () => {
-      const { url, shortcode } = JSON.parse(body);
+    req.on("end", async() => {
+      const { url, shortCode } = JSON.parse(body);
       if (!url) {
         res.writeHead("400", { "content-type": "text/plain" });
         return res.end("URL is required");
       }
 
-      const finalShortCode = shortcode || crypto.randomBytes(4).toString("hex")
+      const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+      if(links[finalShortCode]) {
+        res.writeHead(400, {"Content-Type": "text/plain"})
+      return res.end('Shortcode already exist, plz choose another.')
+      }
+
+      links[finalShortCode] = url
+      await saveLinks(links)
+
+      res.writeHead(200, {"Content-Type": "application/json"})
+      res.end(JSON.stringify({success: true, shortCode: finalShortCode}))
 
     });
   }
